9959bcaa38186429541b67b77ed406921bcb1691
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-07-17 15:37:38 -0400

    set up static pages controller

--- react/src/main.js
+++ react/src/main.js
@@ -2,9 +2,9 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-$(function() {
+document.addEventListener('DOMContentLoaded',function() {
   ReactDOM.render(
     <h1>Boo yaa</h1>,
     document.getElementById('app')
   );
-});
+})
