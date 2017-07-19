8a6222bd3970c598f7daf3aedc87dcb22cf4bcd1
Amanda Beiner <aebeiner@gmail.com>
2017-07-19 14:53:35 -0400

    magic?

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -5,8 +5,16 @@ class Api::V1::AlbumsController < ApplicationController
   end
 
   def create
-    data = JSON.parse(request.body.read)
-    new_album = Album.create(title: data["title"],  uploader: data["uploader"], date_released: data["date_released"])
-    render json: new_album, adapter: :json
+    new_album = Album.create(album_params)
+    # binding.pry
+    # data = JSON.parse(request.body.read)
+    # new_album = Album.create(title: data["title"],  uploader: data["uploader"], date_released: data["date_released"])
+    render json: new_album
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :uploader_id)
   end
 end
--- spec/controllers/api/v1/albums_controller_spec.rb
+++ spec/controllers/api/v1/albums_controller_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::AlbumsController, type: :controller do
 
-
-
   describe "GET#index" do
     before(:each) do
       bob = create(:user, email: "anything@anything.com")
@@ -39,12 +37,13 @@ RSpec.describe Api::V1::AlbumsController, type: :controller do
   describe "POST#create" do
     let!(:jimmy) { FactoryGirl.create(:user, email: 'me@you.com') }
     let!(:new_album) { FactoryGirl.create(:album, title: 'Round Room', uploader: jimmy) }
-    let!(:new_album_data) { { title: new_album.title, uploader_id: new_album.uploader.id  }.to_json}
+    let!(:new_album_data) { {album: {title: new_album.title, uploader_id: new_album.uploader.id}} }
 
     it "should create a new album" do
+      # post '/api/v1/albums', params: { "title" => new_album.title, "uploader_id" => new_album.uploader.id  }
       # binding.pry
-      new_album_data
-      expect{ post(:create, new_album_data) }.to change{ Album.count }.by 1
+      # new_album_data
+      expect{ post(:create, params: new_album_data) }.to change{ Album.count }.by 1
     end
 
 
