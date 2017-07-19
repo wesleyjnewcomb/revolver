46fa678cb9233521a0c53a67594214cae2e26642
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-07-19 11:15:31 -0400

    set up App component

---
+++ react/src/containers/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>Hello, world!</div>
+    );
+  }
+}
+
+export default App;
