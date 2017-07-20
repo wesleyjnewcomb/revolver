3e0d36bf0d020d9f490965f2f0a5e3fbe57726a3
nm357 <n.mikita3@gmail.com>
2017-07-20 12:01:24 -0400

    created new album React form and its container

--- react/src/containers/App.js
+++ react/src/containers/App.js
@@ -6,6 +6,7 @@ import {
  Link
 } from 'react-router-dom';
 import AlbumsIndexContainer from './AlbumsIndexContainer';
+import AlbumsNewFormContainer from './AlbumsNewFormContainer'
 
 const Home = () => {
   return (
@@ -18,7 +19,9 @@ const App = ({ }) => {
     <Router>
       <div>
         <Route exact path='/' component={Home} />
-        <Route path='/albums' component={AlbumsIndexContainer} />
+        <Route exact path='/albums' component={AlbumsIndexContainer} />
+        <Route path='/albums/new' component={AlbumsNewFormContainer} />
+
       </div>
     </Router>
   )
