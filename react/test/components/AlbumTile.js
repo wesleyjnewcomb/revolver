8e49ed6383ce6ec7ff70a361e2f3bb69bb54e817
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-07-20 15:51:03 -0400

    wrote AlbumTile tests

---
+++ react/test/components/AlbumTile.js
@@ -0,0 +1,31 @@
+import AlbumTile from '../src/components/AlbumTile'
+
+describe('AlbumTile', () => {
+  let wrapper;
+  let album = {
+    id: 1,
+    title: "Revolver",
+    date_released: "1966-08-05",
+    artist: {
+      id: 1,
+      name: "The Beatles"
+    }
+  }
+  beforeEach(() => {
+    wrapper = mount(
+      <AlbumTile
+        album={album}
+      />
+    )
+  });
+
+  it('should render the album title', () => {
+    expect(wrapper.find('.album-title').text()).toEqual('Revolver')
+  })
+  it('should render the artist name', () => {
+    expect(wrapper.find('.album-artist').text()).toEqual('The Beatles')
+  })
+  it('should render the month and year released', () => {
+    expect(wrapper.find('h4').text()).toEqual('Aug 1966')
+  })
+})
