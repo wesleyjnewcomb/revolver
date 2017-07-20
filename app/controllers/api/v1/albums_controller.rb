3addbd430bc3640306c5786ec4a094bfb9169329
daniel-simon <daniel.alexander.simon@gmail.com>
2017-07-20 15:26:25 -0400

    stray character :(

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -17,7 +17,6 @@ class Api::V1::AlbumsController < ApplicationController
       created_artist = true
     end
     @new_album.artist = artist
-º
     if @new_album.save
       render json: @new_album
     else
