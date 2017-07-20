7a07534caca1f2801c623053a25ba14a7b81071f
daniel-simon <daniel.alexander.simon@gmail.com>
2017-07-20 13:55:10 -0400

    updates to albums index page and serializer

--- react/src/components/AlbumTile.js
+++ react/src/components/AlbumTile.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+]
+
 const AlbumTile = ({ album }) => {
+  let dateReleased = new Date(album.date_released)
+  let month = dateReleased.getMonth()
+  let year = dateReleased.getFullYear()
   return (
-    <div className='album-tile'>
+    <div className='album-tile panel row'>
       <h3>{album.title}</h3>
-      <h4>{album.date_released.getMonth()} {album.date_released.getYear()}</h4>
+      <h4>{months[month]} {year}</h4>
     </div>
   )
 }
--- react/src/containers/AlbumsIndexContainer.js
+++ react/src/containers/AlbumsIndexContainer.js
@@ -10,6 +10,23 @@ class AlbumsIndexContainer extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch('/api/v1/albums')
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`;
+        let error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => {
+      this.setState({ albums: response.albums });
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }
+
   render() {
     let albums = this.state.albums.map(album => {
       return (
