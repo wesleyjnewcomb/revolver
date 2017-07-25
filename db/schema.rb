a805070cab4598d1289b98a36be9e966e10cd5db
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-07-25 10:00:30 -0400

    added migration to change date_released to year_released, month_released

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721133748) do
+ActiveRecord::Schema.define(version: 20170725132426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 20170721133748) do
   create_table "albums", force: :cascade do |t|
     t.bigint "uploader_id", null: false
     t.string "title", null: false
-    t.date "date_released"
     t.bigint "artist_id", null: false
+    t.integer "year_released", limit: 2
+    t.integer "month_released", limit: 2
     t.index ["artist_id"], name: "index_albums_on_artist_id"
     t.index ["uploader_id"], name: "index_albums_on_uploader_id"
   end
