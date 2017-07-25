27cef8864a228f837c5b27990de0b9115f03c8d6
daniel-simon <daniel.alexander.simon@gmail.com>
2017-07-25 16:35:20 -0400

    model tests now passing

--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :vote do
-    value { rand(-1..1) }
     user { FactoryGirl.create(:user) }
     review { FactoryGirl.create(:review) }
+
+    value { rand(-1..1) }
   end
 end
