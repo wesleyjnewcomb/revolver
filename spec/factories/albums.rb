e089ba831f64044b6dc541ee459c1d4213376407
Wesley Newcomb <wesleyjnewcomb@gmail.com>
2017-07-25 10:24:39 -0400

    updated factory and seed files

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,7 +1,11 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :album do
     uploader { FactoryGirl.create(:user) }
-    title "Junta"
+    title { Faker::Hipster.word.capitalize }
+    year_released { rand(1960..2030) }
+    month_released { rand(0..11) }
     artist { FactoryGirl.create(:artist) }
   end
 end
