083609b0a510ae3d8f444726ee5935bd2d2c35a4
daniel-simon <daniel.alexander.simon@gmail.com>
2017-07-27 11:42:06 -0400

    tests for destroy action in albums controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :albums, only: [:index, :show, :create, :update] do
+      resources :albums, only: [:index, :show, :create, :update, :destroy] do
         resources :reviews, only: [:index]
       end
-      resources :reviews, only: [:index, :show, :create]
+      resources :reviews, only: [:index, :show, :create, :destroy]
       resources :votes, only: [:create]
     end
   end
