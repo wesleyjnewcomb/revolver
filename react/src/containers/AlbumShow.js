afb4d4a6f935fa2c32fe46b779cba137461f3695
daniel-simon <daniel.alexander.simon@gmail.com>
2017-07-27 15:36:29 -0400

    fixed NaN error in new review score

--- react/src/containers/AlbumShow.js
+++ react/src/containers/AlbumShow.js
@@ -129,7 +129,9 @@ class AlbumShow extends Component {
           rating: response.review.rating,
           body: response.review.body,
           user: response.review.user,
-          score: response.review.score
+          score: 0,
+          current_user_vote: 0,
+          signed_in: true
         }
         this.addNewReviewToState(newReview)
         this.clearForm()
